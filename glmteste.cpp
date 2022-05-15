71a7b975f9b4861b5a54132e12a1fb076658f94f
José Carlos <39632709+kinhosz@users.noreply.github.com>
2022-05-15 16:59:50 -0300

    first commit

---
+++ glmteste.cpp
@@ -0,0 +1,13 @@
+#include <glm/vec3.hpp>
+#include <glm/gtx/io.hpp>
+#include <iostream>
+
+using namespace std;
+using glm::vec3;
+
+int main () {
+    vec3 i_am_zero;
+
+    cout << i_am_zero << endl;
+    return 0;
+}
---
+++ vulkanteste.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <vulkan/vulkan.h>
+
+int main(int argc, char *argv[]) {
+
+    VkInstanceCreateInfo vk_info;
+    VkInstance inst = 0;
+    VkResult res;
+
+    vk_info.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+
+    vk_info.pNext = NULL;
+
+    vk_info.pApplicationInfo = NULL;
+
+    vk_info.enabledLayerCount = 0;
+
+    vk_info.ppEnabledLayerNames = NULL;
+
+    vk_info.enabledExtensionCount = 0;
+
+    vk_info.ppEnabledExtensionNames = NULL;
+
+    res = vkCreateInstance(&vk_info, NULL, &inst);
+
+    if (res != VK_SUCCESS) {
+        // Error!
+        printf("Error %d\n", res);        
+        return 1;
+    };
+
+    printf("Device created: %p\n", inst);
+
+    vkDestroyInstance(inst, NULL);
+    return (EXIT_SUCCESS);
+}
